70008f96e188dd101dfe8738ac481a900fc88340
Saurav Patel <42308271+sara16play@users.noreply.github.com>
2020-07-06 01:42:18 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+
+* {
+  box-sizing: border-box;
+}
+
+.bg-image {
+  /* Full height */
+  height: 100%; 
+  
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+
+/* Images used */
+.img2 { background-image: url("/images/Kanyakumari_-_panoramic_view.png"); }
+
+
+/* Position text in the middle of the page/image */
+.bg-text {
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
+  color: white;
+  font-weight: bold;
+  font: 30px times new roman;
+  border: 5px solid #f1f1f1;
+  width: 90%;
+  padding: 30px;
+  margin:60px auto;
+  text-align:justify;
+}
+
+.container{
+     position:relative;
+     height:100%;
+     width:100%;
+ }
+ .up {
+     position:absolute;
+  }
+
+ .down {
+   position:absolute;
+   height:100%;
+   width:100%;
+  }
+img {
+  width: 50%;
+  text-align: center;
+}
+
+.img-container {
+        text-align: center;
+      }
+
+</style>
+</head>
+<body>
+
+<h1 style="background-color:powderblue;color:red;text-align:center;font-size:40px;">Kanyakumari</h1>
+<div class="img-container">
+<img src="/images/800px-Entrance_view_Vivekananda_rock_temple.jpg" alt="HTML5 Icon" style="width:280px;height:270px;">
+
+
+<img src="/images/KANYAKUMARI.jpg" alt="HTML5 Icon" width="120" height="350">
+<img src="/images/Gandhi_Memorial_Kanyakumari.jpg" alt="HTML5 Icon" style="width:280px;height:270px;">
+
+</div>
+<div class="container">
+ <div class="down">
+
+
+   <!-- place your all the backgrounds div here  -->
+<div class="bg-image img2"></div>
+
+
+  </div>
+ <div class="up">
+ <div class="bg-text">
+
+<h4 style="text-align:center;">Kanyakumari (also known as Cape Comorin) is a town in Kanyakumari District in the state of Tamil Nadu in India. The southernmost town in mainland India, it is sometimes referred to as 'the Land's End'.
+
+A popular tourist destination in India, it is famous for its unique ocean sunrise and moonrise, the 133 feet Thiruvalluvar Statue and Vivekananda Rock Memorial off the coast, and as a pilgrimage centre. Lying at the tip of peninsular India, it is the confluence of the Arabian sea, the Bay of Bengal and the Indian Ocean. It has a coastal line of 71.5 km stretched on the three sides.
+
+The town is situated 20 km south of Nagercoil, the headquarters of Kanyakumari District, and about 90 km south of Thiruvananthapuram, the capital of Kerala.
+
+On the shores of the town is a temple dedicated to Goddess Kanyakumari (the virgin Goddess), after which the town is named. Kanyakumari has been a city since the Sangam period and has been referred to in old Tamil literature and in the accounts of Marco Polo and Ptolemy.
+</h3> 
+</div>
+
+
+</div>
+
+</body>
+</html>
